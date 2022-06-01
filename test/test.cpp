4252045b8b919b3faa516d4cebc3c2489fe77c1c
hih313 <31838134+hihi313@users.noreply.github.com>
2022-06-01 17:40:24 +0800

    complete blur detection & reduction and add some comments

--- test/test.cpp
+++ test/test.cpp
@@ -20,15 +20,77 @@ double varianceOfLaplacian(const Mat& src, Mat *lap_result = NULL)
     return focusMeasure;
 }
 
+void calcPSF(Mat& outputImg, Size filterSize, int R)
+{
+    Mat h(filterSize, CV_32F, Scalar(0));
+    Point point(filterSize.width / 2, filterSize.height / 2);
+    circle(h, point, R, 255, -1, 8);
+    Scalar summa = sum(h);
+    imshow("PSF", h);
+    outputImg = h / summa[0]; // Sum of each channel (only a channel has value here)
+}
+// Shift the spectrum image to make center of image as spectrum origin
+void fftshift(const Mat& inputImg, Mat& outputImg)
+{
+    outputImg = inputImg.clone();
+    int cx = outputImg.cols / 2;
+    int cy = outputImg.rows / 2;
+    Mat q0(outputImg, Rect(0, 0, cx, cy));
+    Mat q1(outputImg, Rect(cx, 0, cx, cy));
+    Mat q2(outputImg, Rect(0, cy, cx, cy));
+    Mat q3(outputImg, Rect(cx, cy, cx, cy));
+    Mat tmp;
+    q0.copyTo(tmp);
+    q3.copyTo(q0);
+    tmp.copyTo(q3);
+    q1.copyTo(tmp);
+    q2.copyTo(q1);
+    tmp.copyTo(q2);
+}
+// Convolve inputImg with H in freq domain
+void filter2DFreq(const Mat& inputImg, Mat& outputImg, const Mat& H)
+{
+    Mat planes[2] = { Mat_<float>(inputImg.clone()), Mat::zeros(inputImg.size(), CV_32F) };
+    Mat complexI;
+    // 2nd channel: to store the complex's term after DFT
+    merge(planes, 2, complexI);
+    dft(complexI, complexI, DFT_SCALE);
+    Mat planesH[2] = { Mat_<float>(H.clone()), Mat::zeros(H.size(), CV_32F) };
+    Mat complexH;
+    merge(planesH, 2, complexH);
+    Mat complexIH;
+    mulSpectrums(complexI, complexH, complexIH, 0);
+    idft(complexIH, complexIH);
+    split(complexIH, planes);
+    outputImg = planes[0];
+}
+// Compute th Hw (Wiener filter)
+void calcWnrFilter(const Mat& input_h_PSF, Mat& output_G, double nsr)
+{
+    Mat h_PSF_shifted;
+    fftshift(input_h_PSF, h_PSF_shifted);
+    Mat planes[2] = { Mat_<float>(h_PSF_shifted.clone()), Mat::zeros(h_PSF_shifted.size(), CV_32F) };
+    Mat complexI;
+    merge(planes, 2, complexI);
+    dft(complexI, complexI);
+    split(complexI, planes);
+    Mat denom;
+    pow(abs(planes[0]), 2, denom);
+    denom += nsr;
+    divide(planes[0], denom, output_G);
+}
+
 int main(int argc, char** argv)
 {
     setbuf(stdout, NULL);
 
     // Kernel size
     Size kernel = Size(9, 9);
+    int R = stoi(argv[1]);
+    int SNR = stoi(argv[2]);
 
     Mat image;
-    image = imread("../datasets/mav0/cam0/data/1403638130995097088.png", IMREAD_GRAYSCALE);
+    image = imread(argv[3], IMREAD_GRAYSCALE);
     // imshow("image", image);
 
     // Blur
@@ -53,11 +115,31 @@ int main(int argc, char** argv)
     imgs.push_back(median);
     for(int i = 0; i < imgs.size(); i++)
     {
-        // Variance of Laplacian
+        // Check if image is blured (by Variance of Laplacian)
         printf("Focusness image[%d]: %f\n", i, varianceOfLaplacian(imgs[i]));
+        // if(i == 0) 
+        // {
+        //     imshow("image[0]", imgs[i]);
+        //     continue;
+        // }
+
+        // Image deblur (by out of focus deblur)
+        Mat img_deblur;
+        // it needs to process even image only
+        Rect roi = Rect(0, 0, imgs[i].cols & -2, imgs[i].rows & -2);
+        //Hw calculation (start)
+        Mat Hw, h;
+        calcPSF(h, roi.size(), R);
+        calcWnrFilter(h, Hw, 1.0 / double(SNR));
+        // filtering (start)
+        filter2DFreq(imgs[i](roi), img_deblur, Hw);
+
+        img_deblur.convertTo(img_deblur, CV_8U);
+        normalize(img_deblur, img_deblur, 0, 255, NORM_MINMAX);
+        imshow(format("deblured image[%d]", i), img_deblur);
     }
 
-
+    
 
     waitKey(0);
     return 0;
