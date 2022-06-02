960a9062632c418c20fcb7e420a747d4998ad734
hih313 <31838134+hihi313@users.noreply.github.com>
2022-06-02 09:43:52 +0800

    Update run.sh

--- test/run.sh
+++ test/run.sh
@@ -1,15 +1,32 @@
 ##!/bin/bash
 
-BINARY_NAME="test_output"
 
-while getopts "mr" opt
+
+BINARY_NAME="test_output"
+R=53
+SNR=5200
+IMG="../datasets/mav0/cam0/data/1403638130995097088.png"
+
+while getopts "R:S:I:mr" opt
 do
   case $opt in
+    R)
+        R=$OPTARG
+        ;;
+    S)
+        SNR=$OPTARG
+        ;;
+    I)
+        IMG=$OPTARG
+        ;;
     m)
         make
         ;;
     r)
-        ./$BINARY_NAME
+        # Enable tracing
+        set -x        
+        ./$BINARY_NAME $R $SNR "$IMG"
+        set +x
         ;;
     \?) 
         echo "Invalid option -$OPTARG" >&2
